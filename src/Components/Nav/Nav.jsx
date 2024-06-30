1ee0ec6b3598fea0873ac1a646c01c86bed59118
MDJAmin <mohammadamindf@gmail.com>
2024-06-30 16:16:25 +0430

    Nike

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,13 +1,23 @@
 import { headerLogo } from "../../assets/images";
 import { hamburger } from "../../assets/icons";
+import { navLinks } from "../../constents";
 
 export default function Nav() {
   return (
-    <header>
-      <nav>
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
         <a href="/">
-          <img src={headerLogo} alt="Logo" />
+          <img src={headerLogo} alt="Logo" width={130} height={29}  />
         </a>
+        <ul className="flex-1 flex justify-center gap-16 items-center max-lg:hidden">
+          {
+            navLinks.map((item) => (
+              <li key={item.label} >
+                <a href={item.label} className="text-lg font-semibold text-white hover:text-primary transition-all"></a>
+                {item.label}
+              </li>
+          }
+        </ul>
         {/* Use the hamburger icon here if needed */}
       </nav>
     </header>
