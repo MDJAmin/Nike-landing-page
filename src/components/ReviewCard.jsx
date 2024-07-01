b1a086656b8364d5b4930b1913f32c246bcb4866
MDJAmin <mohammadamindf@gmail.com>
2024-07-01 16:39:12 +0430

    add Customers sec

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { star } from "../assets/icons";
 export default function ReviewCard({ imgURL, customerName, rating, feedback }) {
   return (
     <div className='flex justify-center items-center flex-col'>
@@ -9,19 +8,12 @@ export default function ReviewCard({ imgURL, customerName, rating, feedback }) {
         className='rounded-full object-cover w-[120px] h-[120px]'
       />
       <p className='mt-6 max-w-sm text-center info-text'>{feedback}</p>
-      <div className='mt-3 flex justify-center items-center gap-2.5'>
-        <img
-          src={star}
-          width={24}
-          height={24}
-          alt='rating star'
-          className='object-contain m-0'
-        />
-        <p className='text-xl font-montserrat text-slate-gray'>({rating})</p>
-      </div>
-      <h3 className='mt-1 font-palanquin text-3xl text-center font-bold'>
+      <div className='mt-3 flex justify-center items-center me-7'>
+        <h3 className='font-palanquin text-3xl text-center font-bold text-blue-500'>
         {customerName}
+        <p className='font-montserrat text-slate-gray'>({rating})</p>
       </h3>
+      </div>
     </div>
   );
 }
